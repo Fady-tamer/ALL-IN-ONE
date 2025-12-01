411db164c0b3422d296d9acaf2b87ba11b3b20a1
Fady-tamer <fadytamer2018@gmail.com>
2025-12-01 23:51:07 +0200

    add delete button on product card in admin page

--- adminPage/admin.php
+++ adminPage/admin.php
@@ -20,7 +20,7 @@ if ($conn->connect_error) {
 $message = '';
 $products = [];
 
-$sql_fetch = "SELECT product_id, name, price, category, image_url FROM Products ORDER BY product_id DESC";
+$sql_fetch = "SELECT product_id, name, price, category, image_url FROM Products ORDER BY product_id";
 $result_fetch = $conn->query($sql_fetch);
 
 if ($result_fetch) {
@@ -29,6 +29,20 @@ if ($result_fetch) {
     }
 } else {
     $message .= "<div class='error-msg'>Error fetching products: " . $conn->error . "</div>";
+}
+
+if (isset($_POST['delete_product'])) {
+    $product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
+
+    $stmt = $conn->prepare("DELETE FROM Products WHERE product_id = ?");
+    $stmt->bind_param("i", $product_id);
+    $stmt->execute();
+    if ($stmt->affected_rows > 0) {
+        $message .= "<div class='success-msg'>Product deleted successfully.</div>";
+        header("Location: admin.php");
+    } else {
+        $message .= "<div class='error-msg'>Error deleting product: " . $conn->error . "</div>";
+    }
 }
 
 $conn->close();
@@ -42,7 +56,7 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <link rel="icon" type="image/x-icon" href="../img/icon.ico">
-    <link rel="stylesheet" href="admin.css">
+    <link rel="stylesheet" href="Admin.css">
 </head>
 
 <body>
@@ -54,6 +68,7 @@ $conn->close();
             <a class="logOut-btn" href="logout.php" class="logout">Logout</a>
         </nav>
     </div>
+    <?php echo $message; ?>
     <div class="products">
         <?php if (!empty($products)): ?>
             <?php foreach ($products as $product): ?>
@@ -69,6 +84,11 @@ $conn->close();
                         <h3><?php echo htmlspecialchars($product['name']); ?></h3>
                         <p class="product-price">$<?php echo number_format($product['price'], 2); ?></p>
                     </div>
+                    <form action="admin.php" method="POST" class="deleteProduct">
+                        <input type="hidden" name="product_id" value="<?php echo $product['product_id']; ?>">
+                        <button type="submit" name="edit_product" class="edit-btn">Edit Product</button>
+                        <button type="submit" name="delete_product" class="delete-btn">Delete Product</button>
+                    </form>
                 </div>
             <?php endforeach; ?>
         <?php endif; ?>
